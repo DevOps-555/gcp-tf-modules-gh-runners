49ae2eb46fd7dfa6fa4678b1150feaf77150f8ca
vivekreddy0808 <vivekreddy0808@gmail.com>
2024-07-27 20:37:10 +0530

    Updated github runners

--- modules/ComputeInstances/gh-runner-instance.tf
+++ modules/ComputeInstances/gh-runner-instance.tf
@@ -1,14 +1,14 @@
-resource "google_compute_instance" "gh_runners_01" {
+resource "google_compute_instance" "gh_runners_02" {
   project = "${var.gcp_project}"
-  name         = "gh-runners01"
+  name         = "gh-runners02"
   machine_type = "e2-medium"
-  zone         = "us-central1-c"
+  zone         = "us-central1-b"
 
-  tags = ["poc-utility"]
+  tags = ["default"]
 
   boot_disk {
     auto_delete = true
-    device_name = "ansible-disk-02"
+    device_name = "gh-runner-disk-01"
 
     initialize_params {
       image = "${var.image}"
@@ -30,21 +30,4 @@ resource "google_compute_instance" "gh_runners_01" {
   metadata = {
     ssh-keys = ""
   }
-
-  # provisioner "remote-exec" {
-  #   connection {
-  #     type        = "ssh"
-  #     user        = "${var.ssh_user}"
-  #     private_key = file(var.private_key_path)
-  #     host        = self.network_interface[0].access_config[0].nat_ip
-  #     timeout     = "1m"
-  #   }
-
-  #   inline = [
-  #     "echo Hello, World!",
-  #     "sudo apt-get update",
-  #     "sudo apt-add-repository ppa:ansible/ansible -y",
-  #     "sudo apt-get install -y ansible"
-  #   ]
-  # }
 }
